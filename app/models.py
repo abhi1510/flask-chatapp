e185b23c40e324534baeaa82f0278d3846452088
Abhinav Kumar <abhik.chy@gmail.com>
2020-05-13 10:45:34 +0530

    refactoring UI

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    is_online = db.Column(db.Boolean, default=False)
+    chats = db.relationship('ChatHistory',backref='author', lazy=True)
 
     def __repr__(self):
         return self.username
@@ -16,3 +18,4 @@ class User(db.Model, UserMixin):
 class ChatHistory(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     message = db.Column(db.String(500), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,40 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask_socketio import send
 
 from app import app, db, socketio
-from app.models import ChatHistory
+from app.models import ChatHistory, User
 
 @app.route('/')
 @login_required
 def index():
     chats = ChatHistory.query.all()
-    return render_template('index.html', chats=chats)
+    users = User.query.all()
+    return render_template('index.html', chats=chats, users=users)
 
     
 ########## SOCKET CODE ##########
 
+@socketio.on('connect')
+def handle_connect():
+    if current_user.is_authenticated:
+        current_user.is_online = True
+        db.session.commit()
+        print('Client Connected - ', current_user.username)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    if current_user.is_authenticated:
+        current_user.is_online = False
+        db.session.commit()
+        print('Client disconnected - ', current_user.username)
+
 @socketio.on('message')
 def handle_message(msg):
-    ch = ChatHistory(message=msg)
+    ch = ChatHistory(message=msg, author_id=current_user.id)
     db.session.add(ch)
     db.session.commit()
-    send(msg, broadcast=True)
+    send({
+        'msg': msg,
+        'author': current_user.username 
+    }, broadcast=True)
